2667619c5075979d574d70bd44bf86ee765f8aa1
Юрий <trefsq@yandex.ru>
2022-06-16 08:11:35 +0300

    16.06.22

---
+++ Дополнительно о переменных и выводе.py
@@ -0,0 +1,14 @@
+my_name = 'Зед шоу'
+my_age = 35 # Это правда!
+ma_haight = 188 # см
+my_weight = 80 # кг
+my_eyes = ' Голубые '
+my_teeth = ' Белые '
+my_hair = 'Коштановые'
+
+print(f"Давайте поговорим о человеке по имени {my_name}.")
+print(f"Его рост составляет{ma_haight}см.")
+print(f"Он весит {my_weight}кг.")
+print("На самом деле это не так много.")
+print(f"У него {my_eyes} глаза и {my_hair} волосы.")
+print(f"Его зубы обычно {my_teeth}, хотя он любит пить кофе.")
